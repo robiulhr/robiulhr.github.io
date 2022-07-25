1f1efca708fe5b2e2d8809f6ce861520d4afb167
robiulhr <rh22012001@gmail.com>
2022-07-25 14:22:57 +0600

    Updates

---
+++ precache-manifest.1cf01a8c7a3d5c55381b1a4f82215ce5.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "85a143156b8efeff2c18",
+    "url": "/static/js/main.85a14315.chunk.js"
+  },
+  {
+    "revision": "804d7d1040d97d624185",
+    "url": "/static/js/1.804d7d10.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3fe177a6ea8714008f640198a2ef2579",
+    "url": "/static/media/img01.3fe177a6.jpg"
+  },
+  {
+    "revision": "1290bb92b0ef7a7d5130ef0e62e3a4a3",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 02.1290bb92.png"
+  },
+  {
+    "revision": "cbfda366c8f382f4b534a983903fcb46",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 04.cbfda366.png"
+  },
+  {
+    "revision": "8dbf6c4795d543a61e95f021af20145c",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 15.8dbf6c47.png"
+  },
+  {
+    "revision": "96a7d9f4c4b377e7477e6e64f908cd08",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 25.96a7d9f4.png"
+  },
+  {
+    "revision": "567b11bedef48e395cdff18f912d2f5f",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 11.567b11be.png"
+  },
+  {
+    "revision": "f8aef076abb7614862bf3b3eaa6107cd",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 13.f8aef076.png"
+  },
+  {
+    "revision": "40d8d20b3a6dc7c38227f943ae121c5b",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 16.40d8d20b.png"
+  },
+  {
+    "revision": "fdccab2bfcef1e14e8cf808629212813",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 18.fdccab2b.png"
+  },
+  {
+    "revision": "71f845525aa4cf4560f78cf56a50b235",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 20.71f84552.png"
+  },
+  {
+    "revision": "9845702a8cc12eb3205a38c92f80aa10",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 22.9845702a.png"
+  },
+  {
+    "revision": "4c3b5e6a87a03178f85bf3ce5b9ea646",
+    "url": "/static/media/img.4c3b5e6a.jpg"
+  },
+  {
+    "revision": "0df73ef8b23d6b02e620d7ad68232945",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 01.0df73ef8.png"
+  },
+  {
+    "revision": "c724b7d7c53a8bf896538fbacb3c9a26",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 03.c724b7d7.png"
+  },
+  {
+    "revision": "f5befdb374fe379bdbcdc6ef836bc90c",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 05 .f5befdb3.png"
+  },
+  {
+    "revision": "94584fcb6e949dacb0f331b2670f4aac",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 07 .94584fcb.png"
+  },
+  {
+    "revision": "c73379bf126f7e8fd26d9491ef5db42c",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 24.c73379bf.png"
+  },
+  {
+    "revision": "f107466e890bf00c2d32d062a01fee7a",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 12.f107466e.png"
+  },
+  {
+    "revision": "0c304e78d35666f90e57e6d50fd2c0d0",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 14.0c304e78.png"
+  },
+  {
+    "revision": "833bc00ef2561b66741e99a6c363c919",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 17.833bc00e.png"
+  },
+  {
+    "revision": "2b1c6bce6b1511cd4d07a3a6e200fc0d",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 19.2b1c6bce.png"
+  },
+  {
+    "revision": "0a3358a2c02834ba635e3fb0e25ba8ae",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 21.0a3358a2.png"
+  },
+  {
+    "revision": "49b015230ecd1a057c018a9e197077ac",
+    "url": "/static/media/I will convert PSD to HTML,Figma to HTML, xd to HTML RESPONSIVE web design project 23.49b01523.png"
+  },
+  {
+    "revision": "a07d1ac80eca5f8e89b2df27042675af",
+    "url": "/static/media/blog1.a07d1ac8.jpg"
+  },
+  {
+    "revision": "e79e23bc96bbd293e16c8553710d196a",
+    "url": "/static/media/blog2.e79e23bc.jpg"
+  },
+  {
+    "revision": "79cca39b7a5a8d2873d60f5a112459dd",
+    "url": "/static/media/blog3.79cca39b.jpg"
+  },
+  {
+    "revision": "c9ce2e10192f0b08fb9a49ae7673472b",
+    "url": "/static/media/blog4.c9ce2e10.jpg"
+  },
+  {
+    "revision": "11795c891c4b917b2338f5bcd3586780",
+    "url": "/index.html"
+  }
+];
